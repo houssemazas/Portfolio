35029d8519345201000d4e6e70331e663ee8e14d
houssemazas <149244548+houssemazas@users.noreply.github.com>
2026-04-19 22:05:22 +0100

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useCallback } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/LOGO.png";
+import logo from "../assets/LOGO.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
